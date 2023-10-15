ce4b2a0301926d6b10c4b8fcc19c11708db1d0aa
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-15 22:33:39 +0100

    task_0: update

--- _printf.c
+++ _printf.c
@@ -9,19 +9,36 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	unsigned int bytes;
-	/* contains the number of characters printted by _printf */
+	unsigned int n = 0; /* number of bytes printted to stdout */
+	char *str;
 
+	/* check if format not point to NULL */
+	if (!format)
+		return (-1);
 	va_start(args, format);
-	bytes = 0;
-	while (*format) /* A loop that iterates through all characteres */
+	while (*format)
+	/* A loop that iterates through all characteres */
 	{
-		if (*format == '%')
-			bytes += specifier_handler(*(++format), args);
+		if (*format == '%') /* if format points to a % sign */
+			switch (*(++format))
+			/* then we check the character that comes after the % */
+			{
+				case 'c':
+					n += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str)
+						n += _putchar(*str++);
+					break;
+				case '%':
+					n += _putchar(*format);
+					break;
+			}
 		else
-			bytes += _putchar(*format);
-		format++;
+			n += _putchar(*format);
+		format++; /* increase by one byte */
 	}
 	va_end(args);
-	return (bytes);
+	return (n);
 }
